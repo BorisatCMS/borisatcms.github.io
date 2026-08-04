e9336f90a2b5e038b8026da0ffc858f68cacb8fa
BeaverStudio2007 <bo4rijst.pv@gmail.com>
2026-08-04 14:27:32 +0200

    asdf

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -1,12 +1,17 @@
 import { render } from "preact-render-to-string"
 import { QuartzComponent, QuartzComponentProps } from "./types"
 import BodyConstructor from "./Body"
+import { componentRegistry } from "./registry"
+import TickerConstructor from "./Ticker"
 import {
   CSSResource,
   JSResource,
   JSResourceToScriptElement,
   StaticResources,
 } from "../util/resources"
+
+const Ticker = TickerConstructor()
+componentRegistry.register("Ticker", TickerConstructor, "core")
 import { FullSlug, RelativeURL, joinSegments, normalizeHastElement } from "../util/path"
 import { clone } from "../util/clone"
 import { Root, Element, ElementContent } from "hast"
@@ -347,6 +352,7 @@ export function renderPage(
     <html lang={lang} dir={direction}>
       <Head {...componentData} />
       <body data-slug={slug} data-basepath={basePath}>
+        <Ticker {...componentData} />
         {frame.css && <style dangerouslySetInnerHTML={{ __html: frame.css }} />}
         <div id="quartz-root" class="page" data-frame={frame.name}>
           <Body {...componentData}>
